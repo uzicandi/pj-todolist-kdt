0ac7eeb473ed76c75f08bf7f9d5d0a5481f55fac
uzicandi <uzicandi@gmail.com>
2024-10-25 00:01:56 +0900

    modified: search

--- src/component/ui/search/Search.tsx
+++ src/component/ui/search/Search.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Search.module.css';
 
-const Search = ({ placeholder }) => {
+const Search = ({ placeholder = '할 일을 입력해주세요' }) => {
   return (
-    <div className={styles.searchContainer}>
-      <input
-        type="text"
-        className={styles.searchInput}
-        placeholder={placeholder || '할 일을 입력해주세요'}
-      />
+    <div className={styles.wrapper}>
+      <div className={styles.shadow}>
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            className={styles.searchInput}
+            placeholder={placeholder}
+          />
+        </div>
+      </div>
     </div>
   );
 };
