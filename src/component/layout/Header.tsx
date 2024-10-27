77a26248ef783a9445242d47da6dd165eac0df37
uzicandi <uzicandi@gmail.com>
2024-10-27 10:56:09 +0900

    add: header

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -3,12 +3,13 @@ import styles from './Header.module.css';
 import LogoIconFull from '../../assets/images/doit.png';
 import LogoIconSimple from '../../assets/images/doit_icon.png'
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
   return (
     <nav className={styles.navbar}>
-      {/* 작은 화면에서는 doit_simple.svg, 큰 화면에서는 doit.svg */}
-      <div className={styles.logoContainer}>
+      <div className={styles.logoContainer} onClick={() => router.push('/')}>
         <picture>
           <source media="(min-width: 744px)" srcSet={LogoIconFull.src} />
           <Image src={LogoIconSimple} alt="do it;" className={styles.logo} />
